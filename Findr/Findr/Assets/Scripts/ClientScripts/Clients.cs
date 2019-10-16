46108500f3b150bd2f558c4e290dfaa5ae99741c
Thomas Pring <pringthomas4@gmail.com>
2019-10-16 11:35:22 -0400

    Added normal clients and updated Clients prefab

--- Findr/Findr/Assets/Scripts/ClientScripts/Clients.cs
+++ Findr/Findr/Assets/Scripts/ClientScripts/Clients.cs
@@ -8,9 +8,7 @@ using UnityEngine;
 public class Clients : ScriptableObject
 {
     public new string name;
+    public string bio;
     public Sprite artwork;
-    
-
-
-
+    public List<Trait> traits = new List<Trait>();
 }
