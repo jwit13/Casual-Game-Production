e2502b9affaa41409363b4f9b521aa54937503d9
Dev <dprasad@knights.ucf.edu>
2019-10-16 12:24:11 -0400

    Basic Swiping set up
    Basic swiping setup and layout for game scene

---
+++ Findr/Findr/Assets/Scripts/GameplayScripts/MatcheeCardScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatcheeCardScript : MonoBehaviour
+{
+    public bool isMouseOver = false;
+
+    private void OnMouseOver()
+    {
+        isMouseOver = true;
+    }
+
+    private void OnMouseExit()
+    {
+        isMouseOver = false;
+    }
+
+
+
+}
