c92629f512aa795857090648f4976e5207a58520
Jess <jessica.withers13@gmail.com>
2019-10-19 20:02:05 -0400

    More UI
    Added buttons, toggles, and the assets into the Unity scene. Added functions for Pause, Resume, Lobby, Quit, and Back. Added toggles for SFX and Music but they still need to be coded.

---
+++ Findr/Findr/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool Paused = false;
+
+    public GameObject pauseMenuUI;
+    public GameObject optionsMenuUI;
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
+    void Pause()
+    {
+        UIManager.menuUp = true;
+
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Paused = true;
+    }
+
+    public void PlayGame()
+    {
+        UIManager.menuUp = false;
+        Time.timeScale = 1f;
+        Paused = false;
+    }
+
+    public void BackToLobby()
+    {
+        pauseMenuUI.SetActive(false);
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public void Options()
+    {
+        pauseMenuUI.SetActive(false);
+        optionsMenuUI.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("quitter!");
+        Application.Quit();
+    }
+
+    public void Back()
+    {
+        if (Paused)
+        {
+            pauseMenuUI.SetActive(true);
+            optionsMenuUI.SetActive(false);
+        }
+    }
+
+    public void ToggleSound()
+    {
+
+    }
+
+    public void ToggleMusic()
+    {
+
+    }
+}
