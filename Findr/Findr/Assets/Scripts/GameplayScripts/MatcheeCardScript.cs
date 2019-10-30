211068dc5a3162b07183af2cbed35d6998a1b196
Thomas Pring <pringthomas4@gmail.com>
2019-10-30 03:54:04 -0400

    Adjusted scaling for UI and added population to Matchee Cards
    Matin Plz love us

--- Findr/Findr/Assets/Scripts/GameplayScripts/MatcheeCardScript.cs
+++ Findr/Findr/Assets/Scripts/GameplayScripts/MatcheeCardScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MatcheeCardScript : MonoBehaviour
 {    public bool isMouseOver = false;
@@ -12,10 +13,20 @@ public class MatcheeCardScript : MonoBehaviour
     public bool rightSwipe = false;
     public bool leftSwipe = false;
 
+    public Image ProfilePic;
+    public Text Name;
+    public Text Bio;
+    public List<Image> Traits;
+
+    public List<ScriptableObject> MatcheeList;
+    public List<Trait> TraitList;
+    public List<int> usedTraitNums;
+
     private void Start()
     {
         rt = GetComponent<RectTransform>();
         DisplayWorldCorners();
+        Populate();
     }
 
     private void DisplayWorldCorners()
@@ -28,10 +39,6 @@ public class MatcheeCardScript : MonoBehaviour
         {
             Debug.Log("World Corners " + i + " : " + v[i]);
         }
-
-
-        
-
     }
 
 
@@ -50,11 +57,54 @@ public class MatcheeCardScript : MonoBehaviour
         }
     }
 
+    private void Populate()
+    {
+        //string currentClient = FindrManager.main.selectedClientName.text;
+        string currentClient = FindrManager.staticSelectedClientName;
+        int randRange = MatcheeList.Count;
+        ScriptableObject matchee;
+        matchee = MatcheeList[UnityEngine.Random.Range(0, randRange)];
+        while (matchee.name == currentClient)
+        {
+            matchee = MatcheeList[UnityEngine.Random.Range(0, randRange)];
+        }
 
+        if (matchee is CustomClients)
+        {
+            CustomClients cMatchee = (CustomClients)matchee;
+            ProfilePic.sprite = cMatchee.artwork;
+            Name.text = cMatchee.name;
+            Bio.text = cMatchee.bio;
+            foreach (Image i in Traits)
+            {
+                int randVal = UnityEngine.Random.Range(0, TraitList.Count);
+                while (usedTraitNums.Contains(randVal))
+                {
+                    randVal = UnityEngine.Random.Range(0, TraitList.Count);
+                }
+                usedTraitNums.Add(randVal);
+                i.color = TraitList[randVal].traitColor;
+            }
 
-
-
-
+        }
+        else if (matchee is NormalClients)
+        {
+            NormalClients nMatchee = (NormalClients)matchee;
+            ProfilePic.sprite = nMatchee.artwork;
+            Name.text = nMatchee.name;
+            Bio.text = nMatchee.bio;
+            foreach (Image i in Traits)
+            {
+                int randVal = UnityEngine.Random.Range(0, TraitList.Count);
+                while (usedTraitNums.Contains(randVal))
+                {
+                    randVal = UnityEngine.Random.Range(0, TraitList.Count);
+                }
+                usedTraitNums.Add(randVal);
+                i.color = TraitList[randVal].traitColor;
+            }
+        }
+    }
 
     public void MouseOver()
     {
